ef90d9701e0ed74580c36a056b54b120b3355e5b
onishi taisei <taisei.onishi@axia-works.com>
2022-02-09 00:13:16 +0900

    first commit

---
+++ android/app/src/main/kotlin/com/example/myproduction/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.myproduction
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:myproduction/markdown.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('production'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (context) {
+                      return MarkDownPage();
+                    },
+                  ),
+                );
+              },
+              child: Text('MarkDown'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
